2f525c63bc46adc0896719e239bbb2f98d4af2ba
12200375DesiArianti <12200375@bsi.ac.id>
2021-09-27 15:34:38 +0700

    tugas pertemuan 2

---
+++ app/Views/halaman/login.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Perpustakaan login</title>
+    </head>
+    <body>
+
+        <hl> Selamat datang di Sistem Informasi Perpustakaan</hl>
+        <p>
+             Ini adalah layanan perpustakaan digital masa kini
+             <a href="<?=base_url( relativePath:'/daftar-member')?>">klik daftar</a> untuk mendaftar menjadi member        </p>
+
+    </body>
+</html>
