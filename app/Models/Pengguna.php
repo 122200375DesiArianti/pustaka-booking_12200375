4aca66fd696bd2e367718e909655f119a60bbb3c
12200375DesiArianti <12200375@bsi.ac.id>
2021-10-20 17:56:31 +0700

    Tugas Pertemuan 5

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -14,5 +14,14 @@ class Pengguna extends Model
     protected $protectFields        = true;
     protected $allowedFields        = ['nama', 'password'];
 
-    
+    /**
+     * Method untuk cek login dari table pengguna
+     * bedasarkan nama dan password
+     * @var $string $user
+     * @var $string pass
+     */
+    public function cekLogin($user, $pass){
+        return $this->where('nama', $user)
+                    ->where('password', md5($pass) )->first();
+    }
 }
--- app/Views/halaman/login.php
+++ app/Views/halaman/login.php
@@ -7,31 +7,50 @@
                     <h3 class="card-title">Login</h3>
         </div>
         <div class="card-body">
-            <div class="form-floating">
-                <input id="txtEmail" class="form-control"
-                placeholder="Email"
-                type="text" name="email">
-                <label for="txtEmail">Email</label>
-        </div>
+        <?php if($error){?>
+            <div class="alet alert-danger">
+                <?=$error?> 
+            </div>
+            <?php }?>
         <div class="form-floating">
-                <input id="txtpass" class="form-control"
-                placeholder="Password"
-                type="password" name="sandi">
-                <label for="txtpass">Kata Sandi</label>
-        </div>
-        <button class="btn btn-primary mt-2">Login</button>
+                    <input id="txtEmail" class="form-control"  
+                            placeholder="Email"
+                            value="<?=$email ?? '' ?>"
+                            type="text" name="email" />
+                    <label for="txtEmail" >Email</label>
+
+                <?php  if($vd?->getError('email')){ ?>
+                    <div class="alert alert-danger">
+                    <?php
+                        echo $vd?->getError('email');
+                    ?>
+                    </div>
+                <?php } ?>
+
+                </div> 
+                <div class="form-floating">
+                    <input id="txtPass" class="form-control" 
+                            placeholder="Password"
+                            value="<?=$sandi ?? '' ?>"
+                            type="password" name="sandi" />
+                    <label for='txtPass'>Kata Sandi</label>   
+
+                <?php if($vd?->getError('sandi')){ ?>    
+                    <div class="alert alert-danger">
+                    <?php
+                        echo $vd?->getError('sandi');
+                    ?>   
+                    </div>  
+                <?php } ?>
 
 
+                </div>
 
-        </div>
-
-                
-
-    </form>
-<h1> MASUKAN EMAIL DAN PASSWORD DENGAN BENAR</h1>
-
-        
-    
-<?=$this->endsection()?>
+                <button class="btn btn-primary mt-2" >Login</button>
+            </div>
+       </div>
+   </div>
+</form>
+<?=$this->endSection()?>
         
   
